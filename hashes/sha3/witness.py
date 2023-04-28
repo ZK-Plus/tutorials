82f98f400ef7196ea1e9e7e7bbcf3062a7c0b81e
alvaro-alonso <alfmail@pm.me>
2023-04-28 14:31:12 +0200

    #29 missing sha3 witness generation

---
+++ hashes/sha3/witness.py
@@ -0,0 +1,16 @@
+import hashlib
+
+
+def sha256(preimage: bytearray):
+    return hashlib.sha3_512(preimage).digest()
+
+def sha3_to_u8_array(val: bytearray) -> list[str]:
+    array_arrays = [val[i] for i in range(0, len(val))]
+    u32_array = [str(x) for x in array_arrays]
+    return u32_array
+
+msg = (5).to_bytes(32, "big", signed=False)
+root = sha256(msg)
+
+print(*sha3_to_u8_array(root), *sha3_to_u8_array(msg))
+
