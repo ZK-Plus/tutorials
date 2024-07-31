20f9e51f4bd4257f75125ccf1f6629658aaecda6
alv-around <8678242+alv-around@users.noreply.github.com>
2024-07-31 11:13:53 +0100

    29 blogpost benchmark of hashing algorithms tutorial (#8)
    * #29 sha256
    
    * #29 sha3 hash
    
    * #29 blake2s hash
    
    * #29 sha256 hash
    
    * #29 missing sha3 witness generation
    
    * #29 add missing sha256 files
    
    * #29 consolidate README in one
    
    * #29 correct sha3 witness script
    
    * #29 rephrase and adjust size of hash and message
    
    * #29 add poseidon hash code example
    
    ---------
    
    Co-authored-by: Alvaro Alonso <alvaro-alonso@users.noreply.github.com>

---
+++ hashes/blake2s/witness.py
@@ -0,0 +1,15 @@
+import hashlib
+
+
+def blake2s(preimage: bytearray):
+    return hashlib.blake2s(preimage).digest()
+
+def blake2s_to_u32_array(val: bytearray) -> list[str]:
+    array_arrays = [val[i:i + 4] for i in range(0, len(val), 4)]
+    u32_array = [str(int.from_bytes(x, 'big', signed=False)) for x in array_arrays]
+    return u32_array
+
+msg = (5).to_bytes(64, "big", signed=False)
+hash = blake2s(msg)
+
+print(*blake2s_to_u32_array(hash), *blake2s_to_u32_array(msg))
